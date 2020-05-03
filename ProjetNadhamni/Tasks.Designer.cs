8635db1c09325490d6ea18926c0a33eca72b8094
yasminee99 <64501536+yasminee99@users.noreply.github.com>
2020-05-03 22:00:42 +0200

    first committ

---
+++ ProjetNadhamni/Tasks.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace ProjetNadhamni
+{
+    partial class Tasks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PnlPassage = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_statTasks = new System.Windows.Forms.Button();
+            this.btn_tasksTask = new System.Windows.Forms.Button();
+            this.btn_profileTasks = new System.Windows.Forms.Button();
+            this.btn_dashTasks = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnlDashboard = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.btn_SettingTasks = new System.Windows.Forms.Button();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.ExitDashboard = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExitDashboard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PnlPassage
+            // 
+            this.PnlPassage.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.PnlPassage.Location = new System.Drawing.Point(1, 259);
+            this.PnlPassage.Name = "PnlPassage";
+            this.PnlPassage.Size = new System.Drawing.Size(9, 30);
+            this.PnlPassage.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(50, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 18);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "NADHAMNI";
+            // 
+            // btn_statTasks
+            // 
+            this.btn_statTasks.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_statTasks.FlatAppearance.BorderSize = 0;
+            this.btn_statTasks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_statTasks.Font = new System.Drawing.Font("Lucida Sans", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_statTasks.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_statTasks.Location = new System.Drawing.Point(47, 304);
+            this.btn_statTasks.Name = "btn_statTasks";
+            this.btn_statTasks.Size = new System.Drawing.Size(147, 23);
+            this.btn_statTasks.TabIndex = 6;
+            this.btn_statTasks.Text = "  Statistics";
+            this.btn_statTasks.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btn_statTasks.UseVisualStyleBackColor = true;
+            this.btn_statTasks.Click += new System.EventHandler(this.btn_statistics_Click);
+            // 
+            // btn_tasksTask
+            // 
+            this.btn_tasksTask.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_tasksTask.FlatAppearance.BorderSize = 0;
+            this.btn_tasksTask.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_tasksTask.Font = new System.Drawing.Font("Lucida Sans", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_tasksTask.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_tasksTask.Location = new System.Drawing.Point(47, 266);
+            this.btn_tasksTask.Name = "btn_tasksTask";
+            this.btn_tasksTask.Size = new System.Drawing.Size(147, 23);
+            this.btn_tasksTask.TabIndex = 4;
+            this.btn_tasksTask.Text = "  Tasks";
+            this.btn_tasksTask.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btn_tasksTask.UseVisualStyleBackColor = true;
+            // 
+            // btn_profileTasks
+            // 
+            this.btn_profileTasks.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_profileTasks.FlatAppearance.BorderSize = 0;
+            this.btn_profileTasks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_profileTasks.Font = new System.Drawing.Font("Lucida Sans", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_profileTasks.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_profileTasks.Location = new System.Drawing.Point(47, 228);
+            this.btn_profileTasks.Name = "btn_profileTasks";
+            this.btn_profileTasks.Size = new System.Drawing.Size(147, 23);
+            this.btn_profileTasks.TabIndex = 2;
+            this.btn_profileTasks.Text = "  Profile";
+            this.btn_profileTasks.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btn_profileTasks.UseVisualStyleBackColor = true;
+            this.btn_profileTasks.Click += new System.EventHandler(this.btn_profile_Click);
+            // 
+            // btn_dashTasks
+            // 
+            this.btn_dashTasks.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_dashTasks.FlatAppearance.BorderSize = 0;
+            this.btn_dashTasks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_dashTasks.Font = new System.Drawing.Font("Lucida Sans", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dashTasks.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_dashTasks.Location = new System.Drawing.Point(47, 192);
+            this.btn_dashTasks.Name = "btn_dashTasks";
+            this.btn_dashTasks.Size = new System.Drawing.Size(147, 23);
+            this.btn_dashTasks.TabIndex = 0;
+            this.btn_dashTasks.Text = "  Dashboard";
+            this.btn_dashTasks.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btn_dashTasks.UseVisualStyleBackColor = true;
+            this.btn_dashTasks.Click += new System.EventHandler(this.btn_dashboard_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(163)))), ((int)(((byte)(194)))));
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(200, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1101, 40);
+            this.panel2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(6, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 18);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Tasks";
+            // 
+            // pnlDashboard
+            // 
+            this.pnlDashboard.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlDashboard.BackColor = System.Drawing.Color.LightBlue;
+            this.pnlDashboard.Location = new System.Drawing.Point(200, 40);
+            this.pnlDashboard.Name = "pnlDashboard";
+            this.pnlDashboard.Size = new System.Drawing.Size(1101, 500);
+            this.pnlDashboard.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(173)))), ((int)(((byte)(226)))));
+            this.panel1.Controls.Add(this.pictureBox8);
+            this.panel1.Controls.Add(this.btn_SettingTasks);
+            this.panel1.Controls.Add(this.PnlPassage);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox7);
+            this.panel1.Controls.Add(this.ExitDashboard);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.btn_statTasks);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.btn_tasksTask);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.btn_profileTasks);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.btn_dashTasks);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 540);
+            this.panel1.TabIndex = 6;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::ProjetNadhamni.Properties.Resources.wrench;
+            this.pictureBox8.Location = new System.Drawing.Point(16, 333);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(35, 30);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 20;
+            this.pictureBox8.TabStop = false;
+            // 
+            // btn_SettingTasks
+            // 
+            this.btn_SettingTasks.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_SettingTasks.FlatAppearance.BorderSize = 0;
+            this.btn_SettingTasks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_SettingTasks.Font = new System.Drawing.Font("Lucida Sans", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_SettingTasks.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_SettingTasks.Location = new System.Drawing.Point(47, 333);
+            this.btn_SettingTasks.Name = "btn_SettingTasks";
+            this.btn_SettingTasks.Size = new System.Drawing.Size(147, 30);
+            this.btn_SettingTasks.TabIndex = 19;
+            this.btn_SettingTasks.Text = "  Settings";
+            this.btn_SettingTasks.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btn_SettingTasks.UseVisualStyleBackColor = true;
+            this.btn_SettingTasks.Click += new System.EventHandler(this.btn_parameters_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::ProjetNadhamni.Properties.Resources.calendar__1_;
+            this.pictureBox7.Location = new System.Drawing.Point(62, 21);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 12;
+            this.pictureBox7.TabStop = false;
+            // 
+            // ExitDashboard
+            // 
+            this.ExitDashboard.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitDashboard.Image = global::ProjetNadhamni.Properties.Resources.people;
+            this.ExitDashboard.Location = new System.Drawing.Point(148, 484);
+            this.ExitDashboard.Name = "ExitDashboard";
+            this.ExitDashboard.Size = new System.Drawing.Size(35, 30);
+            this.ExitDashboard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ExitDashboard.TabIndex = 11;
+            this.ExitDashboard.TabStop = false;
+            this.ExitDashboard.Click += new System.EventHandler(this.ExitDashboard_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::ProjetNadhamni.Properties.Resources.bar_chart;
+            this.pictureBox4.Location = new System.Drawing.Point(16, 297);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(35, 30);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ProjetNadhamni.Properties.Resources.checklist;
+            this.pictureBox3.Location = new System.Drawing.Point(16, 259);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(35, 30);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ProjetNadhamni.Properties.Resources.user__3_;
+            this.pictureBox2.Location = new System.Drawing.Point(16, 221);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 30);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjetNadhamni.Properties.Resources.construction;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 185);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 7;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // Tasks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1301, 540);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnlDashboard);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Tasks";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tasks";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExitDashboard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlPassage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox ExitDashboard;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button btn_statTasks;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btn_tasksTask;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btn_profileTasks;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_dashTasks;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel pnlDashboard;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Button btn_SettingTasks;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+    }
+}
